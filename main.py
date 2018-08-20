00b00f26afc9e84a584465feb9d44a4d43ca8d9a
Vasyl Voityk <fastfuse@LMIT-JArne.localdomain>
2018-08-20 15:48:42 +0300

    prefare to send random uid

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from machine import Pin
 import time
+import random
 import urequests as requests
 import wifi_connect
 
 
 # Many ESP8266 boards have active-low "flash" button on GPIO0.
 button = Pin(0, Pin.IN)
-URL = "https://jsonplaceholder.typicode.com/users/1"
+led = Pin(1, Pin.OUT)
+
+TOKENS = []
+# URL = "https://jsonplaceholder.typicode.com/users/1"
+URL = "http://"
 
 
 def main():
@@ -15,20 +20,36 @@ def main():
     """
     print("Main loop started...")
 
+    # connec to wifi network
     wifi_connect.connect()
 
     while True:
+        # button loop
         while True:
+            # wait for button click
             if button.value() == 0:
                 break
+
+            led.on()
             time.sleep_ms(20)
 
         print("Button pressed. Sending request...")
 
-        r = requests.get(URL)
+        # r = requests.get(URL)
+        r = requests.post(URL, data={'uid': random.choice(TOKENS)})
 
         print("Response status: {}".format(r.status_code))
-        print("Response data: {}".format(r.json()))
+        # print("Response data: {}".format(r.json()))
+
+        if r.status_code != 200:
+            print("Error")
+            print(r.json())
+
+        else:
+            print("Success")
+            print(r.json())
+
+            led.off()
 
         # It's mandatory to close response objects as soon as you finished
         # working with them. On MicroPython platforms without full-fledged
--- wifi_connect.py
+++ wifi_connect.py
@@ -6,7 +6,6 @@ def connect():
     """
     Function to connect to WIFI network.
     """
-
     import network
     sta_if = network.WLAN(network.STA_IF)
     ap_if = network.WLAN(network.AP_IF)
