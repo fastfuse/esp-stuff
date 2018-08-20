f6241def898ab0bf107c7ed8a9ecf1577815cdc8
fastfuse <banderstadt251@gmail.com>
2018-08-20 08:25:03 +0300

    wip

--- main.py
+++ main.py
@@ -38,5 +38,6 @@ def main():
         time.sleep_ms(200)
 
 
+
 if __name__ == "__main__":
     main()
--- wifi_connect.py
+++ wifi_connect.py
@@ -9,9 +9,11 @@ def connect():
 
     import network
     sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
 
     with open('wlan') as creds:
-        ssid, pswd = creds.read().splitines()
+        # ssid, pswd = creds.read().splitlines()
+        ssid, pswd = [line.strip() for line in creds.readlines()]
 
     if not sta_if.isconnected():
         print('Connecting to network...')
@@ -21,5 +23,7 @@ def connect():
 
         while not sta_if.isconnected():
             pass
+
+    ap_if.active(False)
 
     print('Connected. Network config:', sta_if.ifconfig())
